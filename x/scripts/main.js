e87a68fe48a79951d96f5ba3a085dabbb062eb9f
ShengTaChung <schung21@masonlive.gmu.edu>
2016-06-11 19:50:03 -0400

    d

--- x/scripts/main.js
+++ x/scripts/main.js
@@ -1,4 +1,5 @@
 requirejs(["node_modules/request/request"], function(request){
+	console.log("hi");
 	var request = require('request');
 	request('https://api.uber.com/v1/estimates/price?start_latitude=37.625732&start_longitude=-122.377807&end_latitude=37.785114&end_longitud    e=-122.406677&server_token=Bzpj2OqdsiEfG8gXzI_UcoBY76hhfxiPF0CsOs3Q', function(error, response, body)
      {
