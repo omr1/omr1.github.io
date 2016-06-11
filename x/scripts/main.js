dcecb0c7daaca50a90f0bfb8ecd3849abb533e85
ShengTaChung <schung21@masonlive.gmu.edu>
2016-06-11 19:38:55 -0400

    d

---
+++ x/scripts/main.js
@@ -0,0 +1,12 @@
+requirejs(["node_modules/request/request"], function(request){
+	var request = require('request');
+	request('https://api.uber.com/v1/estimates/price?start_latitude=37.625732&start_longitude=-122.377807&end_latitude=37.785114&end_longitud    e=-122.406677&server_token=Bzpj2OqdsiEfG8gXzI_UcoBY76hhfxiPF0CsOs3Q', function(error, response, body)
+     {
+         if(!error && response.statusCode == 200)
+         {
+             var json = JSON.parse(body);
+             console.log(json);
+        }
+     }
+     );
+}); 
