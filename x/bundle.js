23821b39a7fd6159f2c87684cf773c34155ceba9
ShengTaChung <schung21@masonlive.gmu.edu>
2016-06-12 09:02:25 -0400

    d

--- x/bundle.js
+++ x/bundle.js
@@ -3,9 +3,9 @@
 console.log("i");
 	console.log("hi");
 	var request = require('request');
-	request('https://api.uber.com/v1/estimates/price?start_latitude=37.625732&start_longitude=-122.377807&end_latitude=37.785114&end_longitude=-122.406677&server_token=Bzpj2OqdsiEfG8gXzI_UcoBY76hhfxiPF0CsOs3Q', function(error, response, body)
+	request('https://api.uber.com/v1/estimates/price?start_latitude=37.625732&start_longitude=-122.377807&end_latitude=37.785114&end_longitude=-122.406677&server_token=vgQFxpG0Xp9TjhsKeOZvEcIZNEE8gfCIAbufrFg0', function(error, response, body)
      {
-         if(!error && response.statusCode == 200)
+		if(!error && response.statusCode == 200)
          {
              var json = JSON.parse(body);
              console.log(json);
